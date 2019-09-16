f38d306dc2329630c657ef68b44eb99613cf7472
JosueCortez-UCA <00402918@uca.edu.sv>
2019-09-16 10:02:26 -0600

    FIX: en el commit anterior no agregue los archivos renombrados, ahora si

---
+++ laboratorios/lab2y3/consultas-ucasoft.sql
@@ -0,0 +1,28 @@
+﻿/*1. Obtenga el listado de los nombres y DUI de todos los miembros que
+participaron en la superpachanga que se realizó en 2018.*/
+
+/*2. ¿Qué proyecto es el que ha acumulado más dinero? ¿cuál el que menos?*/
+
+/*3. ¿Cuánto se ha ganado por implantación de los proyectos cada año?*/
+
+/*4. Liste los clientes (todos sus atributos) y muestre por cada uno el monto
+total de todos sus contratos. Ordénelos de mayor a menor por monto.*/
+
+/*5. Obtenga el monto acumulado por tipo de proyectos (web,
+venta_almacen y ERP).*/
+
+/*6. Filtre de la anterior consulta aquellas categorías que tienen ganancias
+acumuladas de más de $100,000.*/
+
+/*7. Obtenga un listado de todos los proyectos en el que se muestre el
+nombre de las superpachangas en las que eventualmente fue
+presentado cada proyecto (o nulo si no lo fue).*/
+
+/*8. Liste los nombres de todos los proyectos que tengan versiones
+superiores a la 1.0.*/
+
+/*9. Averigüe de qué proyectos es subproyecto el denominado “aulavirt”.*/
+
+/*10. Efectúe la consulta anterior considerando que los proyectos de los
+cuales “aulavirt” es supbroyecto podrían ser a su vez subproyectos de
+otros, y así sucesivamente.*/
