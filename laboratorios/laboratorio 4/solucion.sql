d8f3598b9114cb4e44d0749306324be153bc37e1
JosueCortez-UCA <00402918@uca.edu.sv>
2019-10-20 20:13:25 -0600

    ADD: ejercicios 1, 2, 3; terminaos

--- laboratorios/laboratorio 4/solucion.sql
+++ laboratorios/laboratorio 4/solucion.sql
@@ -1,1 +1,25 @@
 -- Ejercicio 1
+sudo mkdir /mnt/saas/
+sudo chown postgres /mnt/saas
+
+sudo su
+su postgres
+psql
+
+CREATE TABLESPACE SAAS LOCATION '/mnt/saas';
+SET default_tablespace = SAAS;
+
+-- Ejercicio 2
+CREATE ROLE powerrepuestos WITH LOGIN CREATEDB;
+CREATE ROLE laatencao WITH LOGIN CREATEDB;
+CREATE ROLE simon WITH LOGIN CREATEDB;
+CREATE ROLE moonbucks WITH LOGIN CREATEDB;
+
+-- Ejercicio 3
+CREATE DATABASE powerrepuestos WITH OWNER powerrepuestos;
+
+CREATE DATABASE laatencao WITH OWNER laatencao;
+
+CREATE DATABASE simon WITH OWNER simon;
+
+CREATE DATABASE moonbucks WITH OWNER moonbucks;
