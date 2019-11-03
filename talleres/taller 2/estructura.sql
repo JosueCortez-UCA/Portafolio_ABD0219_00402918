486b94288474245feec0253c2ac4f67d887d2303
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-03 16:24:56 -0600

    personalizacion del sql

--- talleres/taller 2/estructura.sql
+++ talleres/taller 2/estructura.sql
@@ -10,18 +10,21 @@ drop table if exists alumno cascade;
 create table alumno(
     NIE	char(10) not null,
     nombre varchar(100) not null,
-    constraint pk_alumno primary key (NIE));
+    constraint pk_alumno primary key (NIE)
+);
 
 drop table if exists pago cascade;
 create table pago(
     tipo	char(10) not null,
     numero smallint not null,
-    constraint pk_pago primary key (tipo,numero));
+    constraint pk_pago primary key (tipo,numero)
+);
     
 drop table if exists año cascade;
 create table año(
     numero	smallint not null,
-    constraint pk_año primary key (numero));
+    constraint pk_año primary key (numero)
+);
     
 drop table if exists talonario cascade;
 create table talonario(
@@ -40,31 +43,28 @@ create table talonario(
     constraint fk_talonario_pago foreign key (tipo_pago,numero_pago)
     references pago(tipo,numero) on delete cascade on update cascade,
     constraint fk_talonario_año foreign key (numero_año)
-    references año(numero) on delete cascade on update cascade    
-    );
+    references año(numero) on delete cascade on update cascade
+);
 
--- Particiones de talonario
-    
 drop table if exists docente cascade;
 create table docente(
     DUI	char(10) not null,
     nombre varchar(100) not null,
-    constraint pk_docente primary key (DUI));
-    
--- subclases profesor y estudiante 
+    constraint pk_docente primary key (DUI)
+);
 
 drop table if exists responsable cascade;
 create table responsable(
     DUI	char(10) not null,
     nombre varchar(100) not null,
-    constraint pk_responsable primary key (DUI));
-    
--- subclases empleado (oficina y teléfono UCA) y estudiante
+    constraint pk_responsable primary key (DUI)
+);
 
 drop table if exists nivel cascade;
 create table nivel(
     denominacion	char(10) not null,
-    constraint pk_nivel primary key (denominacion));
+    constraint pk_nivel primary key (denominacion)
+);
 
 drop table if exists grado cascade;
 create table grado(
@@ -73,7 +73,7 @@ create table grado(
     constraint pk_grado primary key (denominacion_nivel,numero),
     constraint fk_grado_nivel foreign key (denominacion_nivel)
     references nivel(denominacion) on delete cascade on update cascade
-    );
+);
 
 drop table if exists seccion cascade;
 create table seccion(
@@ -83,7 +83,7 @@ create table seccion(
     constraint pk_seccion primary key (denominacion_nivel_grado,numero_grado,letra),
     constraint fk_seccion_grado foreign key (denominacion_nivel_grado,numero_grado)
     references grado(denominacion_nivel,numero) on delete cascade on update cascade
-    );
+);
 
 drop table if exists seccionxaula cascade;
 create table seccionxaula(
@@ -93,7 +93,7 @@ create table seccionxaula(
     constraint pk_seccionxaula primary key (denominacion_nivel_grado_seccion,numero_grado_seccion,letra_seccion),
     constraint fk_seccionxaula_seccion foreign key (denominacion_nivel_grado_seccion,numero_grado_seccion,letra_seccion)
     references seccion(denominacion_nivel_grado,numero_grado,letra) on delete cascade on update cascade
-    );
+);
 
 drop table if exists seccionxasignatura cascade;
 create table seccionxasignatura(
@@ -103,11 +103,11 @@ create table seccionxasignatura(
     constraint pk_seccionxasignatura primary key (denominacion_nivel_grado_seccion,numero_grado_seccion,letra_seccion),
     constraint fk_seccionxasignatura_seccion foreign key (denominacion_nivel_grado_seccion,numero_grado_seccion,letra_seccion)
     references seccion(denominacion_nivel_grado,numero_grado,letra) on delete cascade on update cascade
-    );
-    
+);
+
 drop table if exists matricula cascade;
 create table matricula(
-    NIE_alumno	char(10) not null,   
+    NIE_alumno	char(10) not null,
     denominacion_nivel_grado_seccion	char(10) not null,
     numero_grado_seccion  SMALLINT not null,
     letra_seccion       char(1) not null,
@@ -119,13 +119,14 @@ create table matricula(
     references seccion(denominacion_nivel_grado,numero_grado,letra) on delete cascade on update cascade,
     constraint fk_matricula_año foreign key (numero_año)
     references año(numero) on delete cascade on update cascade
-    );
+);
 
 drop table if exists edificio cascade;
 create table edificio(
     denominacion	char(10) not null,
     ubicación varchar(100) not null,
-    constraint pk_edificio primary key (denominacion));
+    constraint pk_edificio primary key (denominacion)
+);
 
 drop table if exists acceso cascade;
 create table acceso(
@@ -138,4 +139,5 @@ create table acceso(
     constraint fk_acceso_alumno foreign key (NIE_alumno)
     references alumno(NIE) on delete cascade on update cascade,
     constraint fk_acceso_edificio foreign key (denominacion_edificio)
-    references edificio(denominacion) on delete cascade on update cascade);
+    references edificio(denominacion) on delete cascade on update cascade
+);
