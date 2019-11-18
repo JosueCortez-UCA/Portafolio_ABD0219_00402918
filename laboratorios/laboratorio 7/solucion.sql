04c19b9e7b72d1ee95cb67e1c53971c86b9653d5
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-17 19:17:50 -0600

    ADD: solucion ejercicio 1

--- laboratorios/laboratorio 7/solucion.sql
+++ laboratorios/laboratorio 7/solucion.sql
@@ -1,15 +1,19 @@
--- 1
+/*
+    1
+*/
 --TABLA cliente
+DROP TABLE cliente CASCADE;
 CREATE TABLE cliente(
 	dui char(10) not null,
 	denominacion varchar(100) not null,
-	tipo varchar(40) not null check (tipo in ('Persona física','Empresa','ONG','Institución pública','Institución académica'))
+	tipo varchar(40) not null check (tipo in ('Persona física','Empresa','ONG','Institución pública','Institución académica')),
 	pais char(2)
 )PARTITION BY LIST(pais);
 
 CREATE TABLE clientes_def PARTITION OF clientes DEFAULT;
 
 --TABLA contrata
+DROP TABLE contrata CASCADE;
 CREATE TABLE contrata(
 	codigo_proyecto char(5) not null,
 	dui_cliente char(10) not null,
@@ -20,9 +24,12 @@ CREATE TABLE contrata(
 	mantenimiento_periodicidad varchar(30) not null,
 	mantenimiento_precio money not null default 0,
     pais char(2)
-);
+)PARTITION BY LIST(pais);
+
+CREATE TABLE contrata_def PARTITION OF contrata DEFAULT;
 
 --TABLA atiende
+DROP TABLE atiende CASCADE;
 CREATE TABLE atiende(
 	codigo_proyecto char(5) not null,
 	dui_cliente char(10) not null,
@@ -30,8 +37,6 @@ CREATE TABLE atiende(
     pais char(2),
 	constraint pk_atiende primary key(codigo_proyecto, dui_cliente, dui_miembro_ventas),
 	constraint fk_atiende_proyecto foreign key(codigo_proyecto) references proyecto(codigo)
-	ON delete cascade ON update cascade,
-	constraint fk_atiende_cliente foreign key(dui_cliente) references cliente(dui)
 	ON delete cascade ON update cascade,
 	constraint fk_atiende_miembro_ventas foreign key(dui_miembro_ventas) references ventas(dui_miembro)
 	ON delete cascade ON update cascade
